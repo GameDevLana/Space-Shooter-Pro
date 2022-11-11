0aac2c8a1ae1c269d4aaec54fb5b94e64653187e
Lana Elfstrom <elfqueenof5@yahoo.com>
2022-11-10 19:09:02 -0800

    Create new Space Shooter Unity Project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.5f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    private float _canfire = -1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+               transform.position = new Vector3(0, 0, 0);
+    }
+    
+    // Update is called oncer per frame
+    void Update()
+    {
+        CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canfire)
+        {
+            FireLaser();
+        }
+
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+      
+        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+
+        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+        if (transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
+    }
+    void FireLaser ()
+    {
+        _canfire = Time.time + _fireRate;
+        Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.75f, 0), Quaternion.identity);
+    }
+}
