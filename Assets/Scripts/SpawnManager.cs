c45271b7cdefee6e5a7c5866f0c99985f08dea88
Lana Elfstrom <elfqueenof5@yahoo.com>
2024-08-17 18:29:05 -0700

    update. working on enemytypes 8.17.24

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,6 +9,7 @@ public class SpawnManager : MonoBehaviour
 
   //[SerializeField]
   //private GameObject[] enemyTypes;
+  
 
     [SerializeField]
     private GameObject _enemyContainer;
@@ -33,17 +34,12 @@ public class SpawnManager : MonoBehaviour
     public int total;
     public int randomNumber;
    
-    void Start()
-    {
-       
-    }
 
     //*********Spawn new enemyLaserBeam within a wave coroutine at a random location. There will be probability assigned later********
 
     public void StartSpawning()
     {
         StartCoroutine(SpawnEnemyRoutine(10, 5.0f));
-       
 
         StartCoroutine(SpawnPowerupRoutine());
     }
@@ -52,87 +48,96 @@ public class SpawnManager : MonoBehaviour
 
     //see below for start of spawn random enemies code
 
-    /*  
-        This is the code for the powerup spawnroutine:
-       
-        yield return new WaitForSeconds(2.0f);
+      
+    //  This is the code for the powerup spawnroutine:
+      /*  yield return new WaitForSeconds(2.0f);
         while (_stopSpawning == false)
         {
             Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
 
             int randomPowerUp = Random.Range(0, 7);
             Instantiate(powerups[randomPowerUp], posToSpawn, Quaternion.identity);
-            yield return new WaitForSeconds(Random.Range(3, 8));
-        }
-    */
-    // https://pastebin.com/ZaY4z4fa
+            yield return new WaitForSeconds(Random.Range(3, 8));*/
+
     IEnumerator SpawnEnemyRoutine(int enemies, float delayToSpawn) //wave one would be normal enemies for now
     {
         yield return new WaitForSeconds(2.0f);
         while (enemies > 0 && _stopSpawning == false)
         {
             Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
-            GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            GameObject newEnemy; if (Random.Range(0, 1f) > 0.5)
+            {
+               newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            }
+            else
+            {
+                newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            }
             newEnemy.transform.parent = _enemyContainer.transform;
-            enemies--;
-            yield return new WaitForSeconds(delayToSpawn);
-        }
-        yield return StartCoroutine(WaveTwoRoutine(15, 3f));
-        yield return StartCoroutine(WaveThreeRoutine(20, 3f));
-        yield return StartCoroutine(WaveFourRoutine(25, 3f));
-        yield return StartCoroutine(WaveFiveRoutine(30, 3f));
-    }
 
-    IEnumerator WaveTwoRoutine(int enemies, float delayToSpawn)    //wave two would be diagonal or zigzag movement
-    {
-        yield return new WaitForSeconds(2.0f);
-        while (enemies < 0 && _stopSpawning == false)
-        {
-            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
-            GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
-            newEnemy.transform.parent = _enemyContainer.transform;
-            enemies--;
-            yield return new WaitForSeconds(delayToSpawn);
-        }
-    }
-    IEnumerator WaveThreeRoutine(int enemies, float delayToSpawn)   //wave three would be unique projectile - laserbeam enemies
-    {
-        yield return new WaitForSeconds(2.0f);
-        while (enemies < 0 && _stopSpawning == false)
-        {
-            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
-            GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
-            newEnemy.transform.parent = _enemyContainer.transform;
-            enemies--;
-            yield return new WaitForSeconds(delayToSpawn);
-        }
-    }
-    
-    IEnumerator WaveFourRoutine(int enemies, float delayToSpawn)    //wave four would be unique projectile - homing
-    {
-        yield return new WaitForSeconds(2.0f);
-        while (enemies < 0 && _stopSpawning == false)
-        {
-            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
-            GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
-            newEnemy.transform.parent = _enemyContainer.transform;
             enemies--;
             yield return new WaitForSeconds(delayToSpawn);
         }
     }
 
-    IEnumerator WaveFiveRoutine(int enemies, float delayToSpawn)    //wave five will be smart enemy with pickup
-    {
-        yield return new WaitForSeconds(2.0f);
-        while (enemies < 0 && _stopSpawning == false)
-        {
-            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
-            GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
-            newEnemy.transform.parent = _enemyContainer.transform;
-            enemies--;
-            yield return new WaitForSeconds(delayToSpawn);
-        }
-    }  
+    /* yield return StartCoroutine(WaveTwoRoutine(15, 3f));
+          yield return StartCoroutine(WaveThreeRoutine(20, 3f));
+          yield return StartCoroutine(WaveFourRoutine(25, 3f));
+          yield return StartCoroutine(WaveFiveRoutine(30, 3f));*/
+
+
+    /*  IEnumerator WaveTwoRoutine(int enemies, float delayToSpawn)    //wave two would be diagonal or zigzag movement
+      {
+          yield return new WaitForSeconds(2.0f);
+          while (enemies < 0 && _stopSpawning == false)
+          {
+              Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+              GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+              newEnemy.transform.parent = _enemyContainer.transform;
+              enemies--;
+              yield return new WaitForSeconds(delayToSpawn);
+          }
+      }*/
+
+    /*  IEnumerator WaveThreeRoutine(int enemies, float delayToSpawn)   //wave three would be unique projectile - laserbeam enemies
+      {
+          yield return new WaitForSeconds(2.0f);
+          while (enemies < 0 && _stopSpawning == false)
+          {
+              Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+              GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+              newEnemy.transform.parent = _enemyContainer.transform;
+              enemies--;
+              yield return new WaitForSeconds(delayToSpawn);
+          }
+       }*/
+
+    /* IEnumerator WaveFourRoutine(int enemies, float delayToSpawn)    //wave four would be unique projectile - homing
+     {
+         yield return new WaitForSeconds(2.0f);
+         while (enemies < 0 && _stopSpawning == false)
+         {
+             Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+             GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+             newEnemy.transform.parent = _enemyContainer.transform;
+             enemies--;
+             yield return new WaitForSeconds(delayToSpawn);
+         }
+     }*/
+
+    /* IEnumerator WaveFiveRoutine(int enemies, float delayToSpawn)    //wave five will be smart enemy with pickup
+     {
+         yield return new WaitForSeconds(2.0f);
+         while (enemies < 0 && _stopSpawning == false)
+         {
+             Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+             GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+             newEnemy.transform.parent = _enemyContainer.transform;
+             enemies--;
+             yield return new WaitForSeconds(delayToSpawn);
+         }
+     }*/
+
 
     IEnumerator SpawnPowerupRoutine()                                       
     {
