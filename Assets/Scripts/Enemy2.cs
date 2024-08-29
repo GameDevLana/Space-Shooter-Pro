33d4d3a9581cf3bab4b16a20d0ef0c336113acc4
Lana Elfstrom <elfqueenof5@yahoo.com>
2024-08-28 20:58:34 -0700

    08.28.24 Finished 2nd Enemy-Diagonal

--- Assets/Scripts/Enemy2.cs
+++ Assets/Scripts/Enemy2.cs
@@ -7,8 +7,8 @@ public class Enemy2 : MonoBehaviour
     [SerializeField]
     private float _speed = 2.5f;
 
-    //[SerializeField]
-    //private GameObject _laserPrefab;
+    [SerializeField]
+    private GameObject _laserPrefab;
 
     private Player _player;
 
@@ -16,8 +16,8 @@ public class Enemy2 : MonoBehaviour
 
     private AudioSource _audioSource;
 
-    //private float _fireRate = 3.0f;
-    //private float _canFire = -1;
+    private float _fireRate = 3.0f;
+    private float _canFire = -1;
 
     private void Start()
     {
@@ -41,31 +41,16 @@ public class Enemy2 : MonoBehaviour
     {
         DiagonalMovement();
 
-       /* if (Time.time > _canFire)
+        if (Time.time > _canFire)
         {
             _fireRate = Random.Range(3f, 7f);
             _canFire = Time.time + _fireRate;
-            GameObject enemyLaser = Instantiate(_laserPrefab, transform.position, Quaternion.identity);
-            Laser[] lasers = enemyLaser.GetComponentsInChildren<Laser>();
-
-            for (int i = 0; i < lasers.Length; i++)
-            {
-                lasers[i].AssignEnemyLaser();
-            }
-        }*/
-    }
-   /* 
-    * void CalculateMovement()
-    {
-        transform.Translate(Vector3.down * _speed * Time.deltaTime);
-
-        if (transform.position.y < -5f)
-        {
-            float randomX = Random.Range(-8f, 8f);
-            transform.position = new Vector3(randomX, 7, 0);
+            GameObject enemyLaser = Instantiate(_laserPrefab, transform.position + new Vector3(0, -1.2f, 0), Quaternion.identity);
+            Laser laser = enemyLaser.GetComponent<Laser>();
+            laser.AssignEnemyLaser();
         }
     }
-   */
+
     void DiagonalMovement()  //Enemy Two - diagonal movement
     {
         transform.Translate(new Vector3(1, -3, 0).normalized * _speed * Time.deltaTime);
@@ -76,7 +61,6 @@ public class Enemy2 : MonoBehaviour
             transform.position = new Vector3(randomX, 7, 0);
         }
     }
-
 
     private void OnTriggerEnter2D(Collider2D other)
     {
